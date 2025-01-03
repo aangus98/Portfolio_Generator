314d6da0907a4359a86bdcd970d543dc142fcdb4
Andrew <mrangus298@gmail.com>
2025-01-03 10:20:50 -0600

    Updated index.css, navigation.jsx, and header.jsx

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header.js';
+import Header from './components/Header';
 import Footer from './components/Footer';
 import AboutMe from './components/AboutMe';
-import Portfolio from './components/Portfolio.jsx';
+import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
 
@@ -27,3 +27,4 @@ function App() {
 }
 
 export default App;
+
--- Portfolio/src/components/Header.jsx
+++ Portfolio/src/components/Header.jsx
@@ -1,33 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Navigation } from './Navigation';
 
 function Header() {
     return (
         <header>
             <h1>Developer's Name</h1>
             <nav>
-                <ul>
-                    <li>
-                        <NavLink to="/" end activeClassName="active">
-                            About Me
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/portfolio" activeClassName="active">
-                            Portfolio
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/contact" activeClassName="active">
-                            Contact
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/resume" activeClassName="active">
-                            Resume
-                        </NavLink>
-                    </li>
-                </ul>
+                <Navigation />
             </nav>
         </header>
     );
